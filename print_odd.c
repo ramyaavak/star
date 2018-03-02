ac0215117dcd05fb3e65e5749dbe04d2723ffc0e
ramyaavak <35220550+ramyaavak@users.noreply.github.com>
2018-03-02 03:29:02 -0800

    print_odd.c
    print_odd

---
+++ print_odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char k[10];
+int i,n;
+scanf("%s",k[i]);
+n=strlen(k);
+for(i=0;i<=n;i++)
+{
+if(k[i]%2!=0)
+printf("%d\t",i);
+}
+return 0;
+}
